a8f5b467c11dcbe319165e0e7ba74287b8ad1290
Brandon Adame <badamegachuz@gmail.com>
2025-03-05 10:09:48 -0500

    Adding temporary landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,9 +10,16 @@ module.exports = {
     "./src/**/*.{astro,astro:jsx,astro:jsx-dev-runtime,astro:tsx,astro:tsx-dev-runtime,html,js,jsx,ts,tsx,vue}",
   ],
   theme: {
+    fontFamily: {
+      "site-main-font": ["Kotta One", "sans-serif"],
+    },
     extend: {
       colors: {
         "primary-site-color": "#F4F1BB",
+        "earth-orange": "#ED6A5A",
+        "light-blue": "#9BC1BC",
+        "dark-blue": "#5CA4A9",
+        "site-gray": "#E6EBE0",
       },
     },
   },
