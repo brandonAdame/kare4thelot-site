d857140d9591a3897d74244705567e286a06e87c
Brandon Adame <badamegachuz@gmail.com>
2025-04-11 12:44:29 -0400

    Navigating to upcoming events when user clicks button

--- src/components/animata/button/slide-arrow-button.tsx
+++ src/components/animata/button/slide-arrow-button.tsx
@@ -14,21 +14,23 @@ export default function SlideArrowButton({
   ...props
 }: SlideArrowButtonProps) {
   return (
-    <button
-      className={`group relative rounded-full border border-white bg-white p-2 text-xl font-semibold ${className}`}
-      {...props}
-    >
-      <div
-        className="absolute left-0 top-0 flex h-full w-11 items-center justify-end rounded-full transition-all duration-200 ease-in-out group-hover:w-full"
-        style={{ backgroundColor: primaryColor }}
+    <a href="/upcomingevents">
+      <button
+        className={`group relative rounded-full border border-white bg-white p-2 text-xl font-semibold ${className}`}
+        {...props}
       >
-        <span className="mr-3 text-white transition-all duration-200 ease-in-out">
-          <ArrowRight size={20} />
+        <div
+          className="absolute left-0 top-0 flex h-full w-11 items-center justify-end rounded-full transition-all duration-200 ease-in-out group-hover:w-full"
+          style={{ backgroundColor: primaryColor }}
+        >
+          <span className="mr-3 text-white transition-all duration-200 ease-in-out">
+            <ArrowRight size={20} />
+          </span>
+        </div>
+        <span className="relative left-4 z-10 whitespace-nowrap px-8 font-semibold text-black transition-all duration-200 ease-in-out group-hover:-left-3 group-hover:text-white">
+          {text}
         </span>
-      </div>
-      <span className="relative left-4 z-10 whitespace-nowrap px-8 font-semibold text-black transition-all duration-200 ease-in-out group-hover:-left-3 group-hover:text-white">
-        {text}
-      </span>
-    </button>
+      </button>
+    </a>
   );
 }
